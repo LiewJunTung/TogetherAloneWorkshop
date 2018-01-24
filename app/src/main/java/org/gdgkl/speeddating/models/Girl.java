919527a6e47bf5ae9bcaa7e79a8a4449d69ecf57
Liew Jun Tung <pandawarrior91@gmail.com>
2018-01-25 00:01:01 +0800

    Added Contracts for Presenters and Models

--- app/src/main/java/org/gdgkl/speeddating/models/Girl.java
+++ app/src/main/java/org/gdgkl/speeddating/models/Girl.java
@@ -6,4 +6,22 @@ package org.gdgkl.speeddating.models;
 
 public class Girl extends Person {
 
+    private String whoPaysTheBill;
+
+    public Girl(String name, int age, String hobby, String phoneNumber, String favouriteMovie, boolean likesHistory, boolean likesDog, int initialScone, String whoPaysTheBill) {
+        super(name, age, hobby, phoneNumber, favouriteMovie, likesHistory, likesDog, initialScone);
+        this.whoPaysTheBill = whoPaysTheBill;
+    }
+
+    public String getWhoPaysTheBill() {
+        return whoPaysTheBill;
+    }
+
+    public String toString() {
+
+        return super.toString() +
+                "Girl{" +
+                "whoPaysTheBill='" + whoPaysTheBill + '\'' +
+                '}';
+    }
 }
